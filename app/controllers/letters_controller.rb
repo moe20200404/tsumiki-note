da09259ac1b6b70a2dc5390567fd72e6b9d5f52d
moe20200404 <98138987+moe20200404@users.noreply.github.com>
2022-05-29 11:57:11 +0900

    おたより削除機能の実装

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -33,6 +33,12 @@ class LettersController < ApplicationController
     end
   end
 
+  def destroy
+    @letter = Letter.find(params[:id])
+    @letter.destroy
+    redirect_to root_path
+  end
+
   private
 
   def letter_params
