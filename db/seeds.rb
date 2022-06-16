b212e69f18b1d54501efad08e0edac902d6371ae
moe20200404 <98138987+moe20200404@users.noreply.github.com>
2022-06-16 22:09:58 +0900

    Seedデータの一部マスク

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,61 +49,61 @@ grades = [
 Grade.import grades
 
 
-# 園児レコードの生成
-kids = []
-r = Random.new
+# # 園児レコードの生成
+# kids = []
+# r = Random.new
 
-# 0歳児クラス
-10.times do |i|
-  kids << Kid.new(
-    name: "大江　園児#{i+1}",
-    gender_id: "#{rand(1..2)}",
-    start_month: "202204",
-    end_month: nil,
-    grade_id: 1,
-    birth_date: "#{Date.jd(rand(2459307..2459671))}",
-    user_id: "#{i+4}"
-  )
-end
+# # 0歳児クラス
+# 10.times do |i|
+#   kids << Kid.new(
+#     name: "大江　園児#{i+1}",
+#     gender_id: "#{rand(1..2)}",
+#     start_month: "202204",
+#     end_month: nil,
+#     grade_id: 1,
+#     birth_date: "#{Date.jd(rand(2459307..2459671))}",
+#     user_id: "#{i+4}"
+#   )
+# end
 
-# 1歳児クラス
-10.times do |i|
-  kids << Kid.new(
-    name: "大江　園児#{i+11}",
-    gender_id: "#{rand(1..2)}",
-    start_month: "202104",
-    end_month: nil,
-    grade_id: 2,
-    birth_date: "#{Date.jd(rand(2458942..2459306))}",
-    user_id: "#{i+14}"
-  )
-end
+# # 1歳児クラス
+# 10.times do |i|
+#   kids << Kid.new(
+#     name: "大江　園児#{i+11}",
+#     gender_id: "#{rand(1..2)}",
+#     start_month: "202104",
+#     end_month: nil,
+#     grade_id: 2,
+#     birth_date: "#{Date.jd(rand(2458942..2459306))}",
+#     user_id: "#{i+14}"
+#   )
+# end
 
-# 2歳児クラス
-5.times do |i|
-  kids << Kid.new(
-    name: "大江　園児#{i+21}",
-    gender_id: "#{rand(1..2)}",
-    start_month: "202004",
-    end_month: nil,
-    grade_id: 3,
-    birth_date: "#{Date.jd(rand(2458576..2458941))}",
-    user_id: "#{i+24}"
-  )
-end
+# # 2歳児クラス
+# 5.times do |i|
+#   kids << Kid.new(
+#     name: "大江　園児#{i+21}",
+#     gender_id: "#{rand(1..2)}",
+#     start_month: "202004",
+#     end_month: nil,
+#     grade_id: 3,
+#     birth_date: "#{Date.jd(rand(2458576..2458941))}",
+#     user_id: "#{i+24}"
+#   )
+# end
 
-# 3歳児クラス
-5.times do |i|
-  kids << Kid.new(
-    name: "大江　園児#{i+26}",
-    gender_id: "#{rand(1..2)}",
-    start_month: "202004",
-    end_month: nil,
-    grade_id: 4,
-    birth_date: "#{Date.jd(rand(2458211..2458575))}",
-    user_id: "#{i+29}"
-  )
-end
+# # 3歳児クラス
+# 5.times do |i|
+#   kids << Kid.new(
+#     name: "大江　園児#{i+26}",
+#     gender_id: "#{rand(1..2)}",
+#     start_month: "202004",
+#     end_month: nil,
+#     grade_id: 4,
+#     birth_date: "#{Date.jd(rand(2458211..2458575))}",
+#     user_id: "#{i+29}"
+#   )
+# end
 
 
-Kid.import kids
+# Kid.import kids
